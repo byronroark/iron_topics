e63377dddbb261666dab03490785feaf2530cdc0
Byron Roark <byronroark@gmail.com>
2015-10-12 02:45:39 -0400

    added action methods

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,72 @@
 class TopicsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @topics = Topics.all
+  end
+
+  def show
+  end
+
+  def search
+    @search = params[:q]
+    @topics = Topics.search(@search)
+
+    render :index
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def edit
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        format.json { render :show, status: :created, location: @topic }
+      else
+        format.html { render :new }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+
+    respond_to do |format|
+      if @topic.update(topic_params)
+        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.json { render :show, status: :ok, location: @topic }
+      else
+        format.html { render :edit }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    respond_to do |format|
+      format.html { redirect_to topic_url, notice: 'Topic was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def topic_params
+    params.require(:topic).permit(:title, :description, :topic_area)
+  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,8 @@ class Topic < ActiveRecord::Base
   validates :description, presence: true
   has_many :interests
   belongs_to :user
+
+  def self.search(search)
+    where("title likes ? or description like ?", "%#{search}%", "%#{search}%")
+  end
 end
