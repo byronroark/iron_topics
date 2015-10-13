1678a6af6d48a4cb0ead9d9084868fc388e49bd5
Byron Roark <byronroark@gmail.com>
2015-10-12 23:44:18 -0400

    comment out ensure logged in (not working)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :ensure_logged_in
-
-  protected
-
-  def ensure_logged_in
-    if current_user.nil?
-      redirect_to new_user_session_path
-    end
-  end
+  # before_action :ensure_logged_in
+  #
+  # protected
+  #
+  # def ensure_logged_in
+  #   if current_user.nil?
+  #     redirect_to new_user_session_path
+  #   end
+  # end
 end
