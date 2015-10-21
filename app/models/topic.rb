86cb6dec82f065d9de0efbf260e5cab77e27a0c8
Byron Roark <byronroark@gmail.com>
2015-10-21 10:44:28 -0400

    add dependent destroy for interests

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   validates :title, presence: true
   validates :focus_area, presence: true
   validates :description, presence: true
-  has_many :interests
+  has_many :interests, dependent: :destroy
   belongs_to :user
 
   accepts_nested_attributes_for :interests
